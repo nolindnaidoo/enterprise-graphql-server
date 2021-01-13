9917b8fba9003bbb895879b3b2b83558f76e56b6
nolindnaidoo <nolin.naidoo@me.com>
2021-01-13 13:28:10 -0500

    add single object query

--- test/object.spec.js
+++ test/object.spec.js
@@ -4,12 +4,12 @@ import chai from 'chai';
 
 const localhost = 'http://localhost:3001';
 
-describe('Query objects', () => {
+describe('Query a single object', () => {
   it('should return response 200 (OK)', (done) => {
     request(localhost)
       .post('/graphql')
       .send({
-        query: `{ objects { id GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        query: `{ object(GraphQLID: 1) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
       })
       .expect(200)
       .end(done);
@@ -19,7 +19,42 @@ describe('Query objects', () => {
     request(localhost)
       .post('/graphql')
       .send({
-        query: `{ objects { id GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        query: `{ object(GraphQLID: 1) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+      })
+      .expect(200)
+      .end((err, res) => {
+        // res will contain array with one user
+        if (err) return done(err);
+        chai.expect(res.body.data).to.deep.equal({
+          object: {
+            GraphQLBoolean: true,
+            GraphQLFloat: 123.4,
+            GraphQLID: '1',
+            GraphQLInt: 1,
+            GraphQLString: 'Array Object 1'
+          }
+        });
+        return done();
+      });
+  });
+});
+
+describe('Query all objects', () => {
+  it('should return response 200 (OK)', (done) => {
+    request(localhost)
+      .post('/graphql')
+      .send({
+        query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+      })
+      .expect(200)
+      .end(done);
+  });
+
+  it('should correctly transform data ouput', (done) => {
+    request(localhost)
+      .post('/graphql')
+      .send({
+        query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
       })
       .expect(200)
       .end((err, res) => {
