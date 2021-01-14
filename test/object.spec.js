cd5c813334b24e6b7d49a122866a93ccd72963b4
nolindnaidoo <nolin.naidoo@me.com>
2021-01-14 11:40:14 -0500

    add update mutation

--- test/object.spec.js
+++ test/object.spec.js
@@ -7,180 +7,184 @@ import Data from '../data/data.js';
 
 const localhost = 'http://localhost:3001';
 
-describe('Query all objects', () => {
-  it('should return response 200 (OK)', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end(done);
+describe('Queries', () => {
+  describe('Query: get all objects', () => {
+    it('should return response 200 (OK)', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end(done);
+    });
+
+    it('should correctly transform data ouput', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          chai.expect(res.body.data).to.deep.equal({
+            objects: [
+              {
+                GraphQLBoolean: true,
+                GraphQLFloat: 0,
+                GraphQLID: '0',
+                GraphQLInt: 0,
+                GraphQLString: 'Object 0'
+              },
+              {
+                GraphQLBoolean: false,
+                GraphQLFloat: 1,
+                GraphQLID: '1',
+                GraphQLInt: 1,
+                GraphQLString: 'Object 1'
+              }
+            ]
+          });
+          return done();
+        });
+    });
   });
 
-  it('should correctly transform data ouput', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        chai.expect(res.body.data).to.deep.equal({
-          objects: [
-            {
+  describe('Query: get a single object by ID', () => {
+    it('should return response 200 (OK)', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `{ object(GraphQLID: 0) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end(done);
+    });
+
+    it('should return correctly transformed data ouput', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `{ object(GraphQLID: 0) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          chai.expect(res.body.data).to.deep.equal({
+            object: {
               GraphQLBoolean: true,
               GraphQLFloat: 0,
               GraphQLID: '0',
               GraphQLInt: 0,
               GraphQLString: 'Object 0'
-            },
-            {
-              GraphQLBoolean: false,
-              GraphQLFloat: 1,
-              GraphQLID: '1',
-              GraphQLInt: 1,
-              GraphQLString: 'Object 1'
             }
-          ]
+          });
+          return done();
         });
-        return done();
-      });
+    });
   });
 });
 
-describe('Query a single object', () => {
-  it('should return response 200 (OK)', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `{ object(GraphQLID: 0) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end(done);
-  });
+describe('Mutations', () => {
+  describe('Mutation: Add a single object', () => {
+    it('should return response 200 (OK)', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `mutation { addObject( GraphQLBoolean: true, GraphQLFloat: ${
+            Data.length + 1
+          }, GraphQLID: ${Data.length + 1}, GraphQLInt: ${
+            Data.length + 1
+          }, GraphQLString: "Object ${
+            Data.length + 1
+          }") { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end(done);
+    });
 
-  it('should return correctly transformed data ouput', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `{ object(GraphQLID: 0) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        chai.expect(res.body.data).to.deep.equal({
-          object: {
-            GraphQLBoolean: true,
-            GraphQLFloat: 0,
-            GraphQLID: '0',
-            GraphQLInt: 0,
-            GraphQLString: 'Object 0'
-          }
+    it('should return correctly mutated & transformed data ouput', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          chai.expect(res.body.data).to.deep.include({
+            objects: [
+              {
+                GraphQLBoolean: true,
+                GraphQLFloat: 0,
+                GraphQLID: '0',
+                GraphQLInt: 0,
+                GraphQLString: 'Object 0'
+              },
+              {
+                GraphQLBoolean: false,
+                GraphQLFloat: 1,
+                GraphQLID: '1',
+                GraphQLInt: 1,
+                GraphQLString: 'Object 1'
+              },
+              {
+                GraphQLBoolean: true,
+                GraphQLFloat: Data.length + 1,
+                GraphQLID: `${Data.length + 1}`,
+                GraphQLInt: Data.length + 1,
+                GraphQLString: `Object ${Data.length + 1}`
+              }
+            ]
+          });
+          return done();
         });
-        return done();
-      });
-  });
-});
-
-describe('Mutation: Add a single object', () => {
-  it('should return response 200 (OK)', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `mutation { addObject( GraphQLBoolean: true, GraphQLFloat: ${
-          Data.length + 1
-        }, GraphQLID: ${Data.length + 1}, GraphQLInt: ${
-          Data.length + 1
-        }, GraphQLString: "Object ${
-          Data.length + 1
-        }") { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end(done);
+    });
   });
 
-  it('should return correctly mutated & transformed data ouput', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        chai.expect(res.body.data).to.deep.include({
-          objects: [
-            {
-              GraphQLBoolean: true,
-              GraphQLFloat: 0,
-              GraphQLID: '0',
-              GraphQLInt: 0,
-              GraphQLString: 'Object 0'
-            },
-            {
-              GraphQLBoolean: false,
-              GraphQLFloat: 1,
-              GraphQLID: '1',
-              GraphQLInt: 1,
-              GraphQLString: 'Object 1'
-            },
-            {
-              GraphQLBoolean: true,
-              GraphQLFloat: Data.length + 1,
-              GraphQLID: `${Data.length + 1}`,
-              GraphQLInt: Data.length + 1,
-              GraphQLString: `Object ${Data.length + 1}`
-            }
-          ]
+  describe('Mutation: Delete a single object by ID', () => {
+    it('should return response 200 (OK)', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `mutation { deleteObject(GraphQLID: ${
+            Data.length + 1
+          }) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end(done);
+    });
+
+    it('should return correctly mutated & transformed data ouput', (done) => {
+      request(localhost)
+        .post('/graphql')
+        .send({
+          query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
+        })
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          chai.expect(res.body.data).to.deep.include({
+            objects: [
+              {
+                GraphQLBoolean: true,
+                GraphQLFloat: 0,
+                GraphQLID: '0',
+                GraphQLInt: 0,
+                GraphQLString: 'Object 0'
+              },
+              {
+                GraphQLBoolean: false,
+                GraphQLFloat: 1,
+                GraphQLID: '1',
+                GraphQLInt: 1,
+                GraphQLString: 'Object 1'
+              }
+            ]
+          });
+          return done();
         });
-        return done();
-      });
-  });
-});
-
-describe('Mutation: Delete a single object', () => {
-  it('should return response 200 (OK)', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `mutation { deleteObject(GraphQLID: ${
-          Data.length + 1
-        }) { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end(done);
-  });
-
-  it('should return correctly mutated & transformed data ouput', (done) => {
-    request(localhost)
-      .post('/graphql')
-      .send({
-        query: `{ objects { GraphQLBoolean GraphQLFloat GraphQLID GraphQLInt GraphQLString } }`
-      })
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        chai.expect(res.body.data).to.deep.include({
-          objects: [
-            {
-              GraphQLBoolean: true,
-              GraphQLFloat: 0,
-              GraphQLID: '0',
-              GraphQLInt: 0,
-              GraphQLString: 'Object 0'
-            },
-            {
-              GraphQLBoolean: false,
-              GraphQLFloat: 1,
-              GraphQLID: '1',
-              GraphQLInt: 1,
-              GraphQLString: 'Object 1'
-            }
-          ]
-        });
-        return done();
-      });
+    });
   });
 });
--- type/mutation.js
+++ type/mutation.js
@@ -11,8 +11,8 @@ import ObjectType from './object';
 import Data from '../data/data';
 
 const RootMutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'Root Mutation Query',
+  name: 'RootMutation',
+  description: 'RootMutation Query',
   fields: () => ({
     addObject: {
       type: ObjectType,
@@ -36,17 +36,58 @@ const RootMutationType = new GraphQLObjectType({
         return object;
       }
     },
+    updateObject: {
+      type: ObjectType,
+      description: 'Update an object partially',
+      args: {
+        GraphQLID: { type: GraphQLInt },
+        GraphQLBoolean: { type: GraphQLBoolean },
+        GraphQLFloat: { type: GraphQLFloat },
+        GraphQLInt: { type: GraphQLInt },
+        GraphQLString: { type: GraphQLString }
+      },
+      resolve: (parent, args) => {
+        const filteredObject = Data.find(
+          (data) => data.GraphQLID === args.GraphQLID
+        );
+
+        if (args.GraphQLBoolean !== undefined) {
+          filteredObject.GraphQLBoolean = args.GraphQLBoolean;
+        }
+
+        if (args.GraphQLFloat !== undefined) {
+          filteredObject.GraphQLFloat = args.GraphQLFloat;
+        }
+
+        if (args.GraphQLInt !== undefined) {
+          filteredObject.GraphQLInt = args.GraphQLInt;
+        }
+
+        if (args.GraphQLString !== undefined) {
+          filteredObject.GraphQLString = args.GraphQLString;
+        }
+
+        const index = Data.findIndex(
+          (object) => object.GraphQLID === filteredObject.GraphQLID
+        );
+
+        Data[index] = filteredObject;
+
+        return filteredObject;
+      }
+    },
     deleteObject: {
       type: ObjectType,
-      description: 'Delete an object',
+      description: 'Delete an object by ID',
       args: {
         GraphQLID: { type: GraphQLNonNull(GraphQLID) }
       },
       resolve: (parent, args) => {
-        const newArray = Data.filter(
+        const filteredObject = Data.filter(
           (object) => object.GraphQLID !== args.GraphQLID
         );
-        Data.splice(0, Data.length, ...newArray);
+
+        Data.splice(0, Data.length, ...filteredObject);
       }
     }
   })
